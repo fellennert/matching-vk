a36912a65d49d0a6ffffba0c00c482b2d8b7916c
fellennert <felix.lennert@icloud.com>
2020-06-04 00:20:12 +0200

    fuzzy function

--- code/fuzzyjoin.R
+++ code/fuzzyjoin.R
@@ -5,25 +5,57 @@ library(tidyverse)
 library(fuzzyjoin)
 
 # get data
-vk_ru <- read_csv("data/russia_vk_cities.csv")
+vk_ru <- read_csv("data/russia_vk_cities_all.csv")
 pop_ru <- read_csv("data/russia_pop_sizes.csv")
-vk_ua <- read_csv("data/ukraine_vk_cities.csv")
+vk_ua <- read_csv("data/ukraine_vk_cities_all.csv")
 pop_ua <- read_csv("data/ukraine_pop_sizes.csv")
 
-# next steps:
-# create empty df with vk cities
-vk_ru_cities <- vk_ru %>% 
-  select(city = city.title) %>% 
-  filter(!is.na(city))
-
-# join "closest" towns to empty df
-joined_vk_cities <- vk_ru_cities %>%
-  stringdist_inner_join(pop_ru, 
-                        by = c(city = "Name"),
-                        max_dist = 1,
-                        distance_col = "distance") %>% 
-  select(vk_city = city.x, pop_city = Name, distance)
-
-
-
 # write function to join closest cities to tbl
+
+join_iteratively <- function(real_cities_vec, false_cities_vec, max_distance = 3) {
+  
+  real_cities_tbl <- real_cities_vec %>% # create tibble
+    enframe() %>% 
+    select(real_city = value) %>% 
+    filter(!is.na(real_city))
+  
+  false_cities_tbl <- false_cities_vec %>% # create tibble
+    enframe() %>% 
+    select(false_city = value) %>% 
+    filter(!is.na(false_city))
+  
+  i <- 0 # set index for while loop
+  output <- vector("list", length = max_distance) # create list to fill
+  
+  while (i < max_distance) {
+    i <- i + 1
+    output[[i]] <- real_cities_tbl %>% # fill list
+      stringdist_inner_join(false_cities_tbl, # fuzzy join
+                        by = c(real_city = "false_city"),
+                        max_dist = i, # distance 1...max_distance(defaults to 3)
+                        distance_col = "distance") %>% 
+      select(real_city, false_city) %>% # get rid of distance column
+      group_by(real_city) %>% 
+      mutate(id = 1,
+             id = cumsum(id)) %>% # ids for spreading
+      pivot_wider(names_from = id, values_from = false_city) # spread it
+  
+  }
+  return(output)
+}
+
+output_ru <- join_iteratively(pop_ru$Name, vk_ru$city.title, max_distance = 3)
+
+output_merged <- vector("list", length = 3)
+for (i in 1:length(output_ru)) {
+  output_merged[[i]] <- pop_ru %>% 
+    right_join(output_ru[[i]], by = c("Name" = "real_city")) %>% # merge original tibble
+    arrange(desc(Pop2019))
+}
+
+# write csvs for checking in Excel
+
+for (i in 1:length(output_merged)) {
+  name <- paste0("data/cities-distance", i, ".csv")
+  write_csv(output_merged[[i]], name)
+}
